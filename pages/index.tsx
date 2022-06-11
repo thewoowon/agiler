54d015f27fd09beb9537c10a41c4ea8d7e791fa3
thewoowon <thewoowon@naver.com>
2022-06-11 11:07:04 +0900

    20220611_Agiler

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from 'styles/Home.module.css'
 import useTranslation from 'next-translate/useTranslation';
+import { Stack, Button } from '@mui/material'
 
 const Home: NextPage = () => {
 
@@ -19,6 +20,12 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Stack spacing={2} direction="row">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
 
         <h2>{t('common:Japanese')}</h2>
         <h2>{t('common:English')}</h2>
