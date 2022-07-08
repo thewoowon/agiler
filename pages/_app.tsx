1a0bc73d0abd706520337b7210995e585237d897
thewoowon <thewoowon@naver.com>
2022-07-08 13:29:35 +0900

    20220708_AGILER

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,22 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {ThemeProvider} from '@mui/material';
 import {theme} from '../theme';
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri:'https://countries.trevorblades.com',
+  cache : new InMemoryCache(),
+});
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-       <Component {...pageProps} />
-    </ThemeProvider>
-  )
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
+  );
 }
 
 export default MyApp
